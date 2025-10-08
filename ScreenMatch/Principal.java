d0bb2fa2f33f4b6ac892e2f0b528854e5b43bab0
TiagoDesc <nobretiago96@gmail.com>
2025-10-07 23:12:44 -0300

    Putting the fisrt getter

--- ScreenMatch/Principal.java
+++ ScreenMatch/Principal.java
@@ -12,8 +12,7 @@ public class Principal {
         meuFilme.avalia(5);
         meuFilme.avalia(10);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
     }
