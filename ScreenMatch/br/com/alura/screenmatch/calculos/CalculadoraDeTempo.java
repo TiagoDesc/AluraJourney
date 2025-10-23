b0b362455a117d3d06d59fc74b55ec6856291398
TiagoDesc <nobretiago96@gmail.com>
2025-10-22 22:33:51 -0300

    including new method

--- ScreenMatch/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ ScreenMatch/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,5 +1,18 @@
 package br.com.alura.screenmatch.calculos;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+
 public class CalculadoraDeTempo {
+
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void inclui(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
 
 }
