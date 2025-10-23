124c62f6f4bb59b92de10a66badffec712da54ff
TiagoDesc <nobretiago96@gmail.com>
2025-10-23 20:15:11 -0300

    adding new business rule

--- DesafioMusica/br/com/alura/desafiomusica/principal/Principal.java
+++ DesafioMusica/br/com/alura/desafiomusica/principal/Principal.java
@@ -1,8 +1,34 @@
 package DesafioMusica.br.com.alura.desafiomusica.principal;
 
+import DesafioMusica.br.com.alura.desafiomusica.modelos.Musica;
+import DesafioMusica.br.com.alura.desafiomusica.modelos.Podcast;
+
 public class Principal {
 
     public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Sem Coração");
+        minhaMusica.setCantor("Mc Kako");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Ponto de vista");
+        meuPodcast.setApresentador("Joel Jota");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
 
     }
 
