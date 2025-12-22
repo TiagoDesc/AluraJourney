ac42ba76c89a538b57746dbcc6a25f57c1aecea3
TiagoDesc <nobretiago96@gmail.com>
2025-12-22 14:19:15 -0300

    toString and Getters

--- Compras/modelo/Compras.java
+++ Compras/modelo/Compras.java
@@ -9,4 +9,17 @@ public class Compras {
         this.valor = valor;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compras [descricao=" + descricao + ", valor=" + valor + "]";
+    }
+
 }
