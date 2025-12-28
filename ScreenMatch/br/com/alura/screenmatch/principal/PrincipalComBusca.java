890e3a738ca5f8cef58107e571c52f11d4559ec0
TiagoDesc <nobretiago96@gmail.com>
2025-12-27 22:08:24 -0300

    new att

--- ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,14 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para mais informações: ");
+        var busca = sc.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=844368c4";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=creed&apikey=844368c4"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
