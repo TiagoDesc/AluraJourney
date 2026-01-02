558e6dd324108ca483029d519f7dc88029339cef
TiagoDesc <nobretiago96@gmail.com>
2026-01-01 23:59:59 -0300

    fix

--- ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,40 +6,23 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
-
 import com.google.gson.Gson;
-
 import br.com.alura.screenmatch.modelos.Titulo;
 
 public class PrincipalComBusca {
-
     public static void main(String[] args) throws IOException, InterruptedException {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um filme para mais informações: ");
         var busca = sc.nextLine();
-
-        String apiKey = System.getenv("OMDB_API_KEY");
-        if (apiKey == null) {
-            throw new RuntimeException("OMDB_API_KEY não definida como variável de ambiente");
-        }
-
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=" + apiKey;
-
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=844368c4";
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
         String json = response.body();
         System.out.println(json);
-
         Gson gson = new Gson();
         Titulo meuTitulo = gson.fromJson(json, Titulo.class);
         System.out.println(meuTitulo);
-
         sc.close();
     }
 }
