7f818c31d9c541d6c270fc04f4276b7a7c012986
TiagoDesc <nobretiago96@gmail.com>
2026-03-10 22:09:46 -0300

    Record Class

--- ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -8,21 +8,28 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 import com.google.gson.Gson;
 import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um filme para mais informações: ");
         var busca = sc.nextLine();
         String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=844368c4";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
         String json = response.body();
         System.out.println(json);
+
         Gson gson = new Gson();
-        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
-        System.out.println(meuTitulo);
+        // Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
         sc.close();
+
     }
 }
