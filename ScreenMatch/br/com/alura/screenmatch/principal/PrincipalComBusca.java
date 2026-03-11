e1e9d6e81dd57c1ed01cc8247014eed3e65518a6
TiagoDesc <nobretiago96@gmail.com>
2026-03-10 22:10:17 -0300

    att

--- ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ ScreenMatch/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,7 +7,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 import com.google.gson.Gson;
-import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.TituloOmdb;
 
 public class PrincipalComBusca {
