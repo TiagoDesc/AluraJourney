47cafe4b985788b339b00cfa2989275a6fa271a9
TiagoDesc <nobretiago96@gmail.com>
2026-03-20 18:02:41 -0300

    Fixing the toString

--- ScreenMatch/br/com/alura/screenmatch/modelos/Titulo.java
+++ ScreenMatch/br/com/alura/screenmatch/modelos/Titulo.java
@@ -104,7 +104,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "Titulo [nome=" + nome + ", anoDeLancamento=" + anoDeLancamento + "]" + duracaoEmMinutos + " minutos";
+        return "(nome = '" + nome + '\'' +
+                ", anoDeLancamento = " + anoDeLancamento + "," +
+                " duração = " + duracaoEmMinutos + ")";
     }
-
 }
